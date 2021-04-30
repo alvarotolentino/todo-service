4c8fc8f758d59f32517bf51992d7f499b7b71c0a
Alvaro Tolentino <alvarotolentinou@gmail.com>
2021-04-30 16:56:19 -0500

    Added migration and updated github action to run integration test

--- db/01-init.sh
+++ db/01-init.sh
@@ -5,26 +5,26 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
   CREATE DATABASE $APP_DB_NAME;
   GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $APP_DB_USER;
-  \connect $APP_DB_NAME $APP_DB_USER
-  BEGIN;  
-    drop table if exists todo_item;
-    drop table if exists todo_list;
+  # \connect $APP_DB_NAME $APP_DB_USER
+  # BEGIN;  
+  #   drop table if exists todo_item;
+  #   drop table if exists todo_list;
 
 
-    create table todo_list (
-      id serial primary key,
-      title varchar(150)
-    );
+  #   create table todo_list (
+  #     id serial primary key,
+  #     title varchar(150)
+  #   );
 
-    create table todo_item(
-      id serial primary key,
-      title varchar(150) not null,
-      checked boolean not null default false,
-      list_id integer not null,
-      foreign key(list_id) references todo_list(id)
-    );
+  #   create table todo_item(
+  #     id serial primary key,
+  #     title varchar(150) not null,
+  #     checked boolean not null default false,
+  #     list_id integer not null,
+  #     foreign key(list_id) references todo_list(id)
+  #   );
 
-    insert into todo_list (title) values ('List 1'), ('List 2');
-    insert into todo_item (title, list_id) values ('Item 1', 1), ('Item 2', 1), ('Item 1', 2);
-  COMMIT;
+  #   insert into todo_list (title) values ('List 1'), ('List 2');
+  #   insert into todo_item (title, list_id) values ('Item 1', 1), ('Item 2', 1), ('Item 1', 2);
+  # COMMIT;
 EOSQL
